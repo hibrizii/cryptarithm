9a742641dc75e8a599fc6ececfa947aba05bca97
Hibbatullah Hibrizii <dqxhibrizii@gmail.com>
2024-07-10 19:22:48 +0700

    memasukkan semua di git

---
+++ app.js
@@ -0,0 +1,57 @@
+import { cryptQuiz, decryptQuiz, displayQuiz } from "./quizProcess.mjs";
+const quizStage = document.querySelector(".question");
+const answerStage = document.querySelector(".answer");
+const submitBtn = document.querySelector("button.submit");
+function checkAnswer(userInput, trueAnswer) {
+  let isValid = true;
+  let inputIndex = 0;
+  for (let num in trueAnswer) {
+    if (num.toString() != userInput[inputIndex]) isValid = false;
+
+    inputIndex++;
+  }
+  return isValid;
+}
+
+function playGame() {
+  const { cryptedQuiz, chars } = cryptQuiz();
+  for (const key in chars) {
+    const answerHTML = document.createElement("div");
+    answerHTML.innerText = `${chars[key]}: `;
+    const inputHTML = document.createElement("input");
+    inputHTML.setAttribute("type", "number");
+    inputHTML.setAttribute("class", "inputNum");
+    answerHTML.appendChild(inputHTML);
+    answerStage.appendChild(answerHTML);
+  }
+
+  const arrInput = Array.from(answerStage.children);
+  arrInput.forEach((e, i, arr) => {
+    e.children[0].addEventListener("input", (event) => {
+      if (i <= arr.length - 2 && event.target.value.length == 1) {
+        event.target.blur();
+        arr[i + 1].children[0].focus();
+        console.log(event.target.value);
+      }
+    });
+  });
+  submitBtn.addEventListener("click", () => {
+    let userInput = arrInput.map((e) => e.children[0].value);
+    if (!userInput.includes("")) {
+      let textNumQuestion = Array.from(quizStage.querySelectorAll("h2 span"));
+      console.log(textNumQuestion);
+      let decryptedQuiz = decryptQuiz(cryptedQuiz, chars, userInput);
+      let isTrue =
+        checkAnswer(userInput, chars) ||
+        decryptedQuiz[0] + decryptedQuiz[1] == decryptedQuiz[2];
+      if (isTrue) {
+        displayQuiz(decryptedQuiz);
+        textNumQuestion.forEach((e) => (e.style.color = "blue"));
+      } else {
+        textNumQuestion.forEach((e) => (e.style.color = "red"));
+      }
+    }
+  });
+}
+
+playGame();
---
+++ quizProcess.mjs
@@ -0,0 +1,85 @@
+const quizStage = document.querySelector(".question");
+
+function generateNum() {
+  const num1 = Math.floor(Math.random() * (8500 - 500 + 1)) + 500;
+  const num2 = Math.floor(Math.random() * (1500 - 100 + 1)) + 100;
+  return [num1, num2, num1 + num2];
+}
+
+function generateNumQuiz() {
+  let isFor = false;
+  let result;
+  while (!isFor) {
+    let arrNums = generateNum();
+    let set = new Set(arrNums.join("").split(""));
+    let is3Num = arrNums.map((e) => new Set(e.toString()).size >= 3);
+    let isNoDouble0 = arrNums.map(
+      (num) => (num.toString().match(/0/g) || []).length <= 1
+    );
+    let isOke = arrNums[0] != arrNums[1] && set.size < 4;
+    if (isOke && (is3Num[0] || is3Num[1]) && isNoDouble0[0] && isNoDouble0[1]) {
+      result = { arrNums, set };
+      isFor = true;
+    }
+  }
+  return result;
+}
+
+function cryptQuiz() {
+  let { arrNums, set } = generateNumQuiz();
+  let chars = {};
+  let arrSet = Array.from(set);
+  let prevChar = [];
+  for (let i = 0; i < arrSet.length; i++) {
+    let randomChar = Math.floor(Math.random() * (76 - 65 + i)) + 65 + i;
+    randomChar = prevChar.includes(randomChar) ? randomChar + 1 : randomChar;
+    chars[`${arrSet[i]}`] = String.fromCharCode(randomChar);
+    prevChar.push(randomChar);
+  }
+  let cryptedQuiz = arrNums.map((num) =>
+    num
+      .toString()
+      .split("")
+      .map((n) => chars[n])
+      .join("")
+  );
+  displayQuiz(cryptedQuiz);
+  return { cryptedQuiz, chars };
+}
+
+function decryptQuiz(cryptedQuiz, chars, userInput) {
+  let keyObj = {};
+  let indexInput = 0;
+  for (const key in chars) {
+    keyObj[`${chars[key]}`] = userInput[indexInput];
+    indexInput++;
+  }
+  let decryptedQuiz = cryptedQuiz.map((num) =>
+    num
+      .toString()
+      .split("")
+      .map((n) => keyObj[n])
+      .join("")
+  );
+  return decryptedQuiz;
+}
+
+function displayQuiz(quizDisplayed) {
+  let quiz = quizDisplayed.map((num) =>
+    num
+      .split("")
+      .map((c) => `<span>${c}</span>`)
+      .join("")
+  );
+  let quizLength = quizDisplayed.map((e) => (e.length == 4 ? "for" : "three"));
+  let htmlQuiz = `
+    <div class="quiz-line">
+     <h2 class="${quizLength[0]}">${quiz[0]}</h2>
+     <h2 class="${quizLength[1]}">${quiz[1]}</h2>
+    </div>
+     <h2 class="${quizLength[2]}">${quiz[2]}</h2>
+    `;
+  quizStage.innerHTML = htmlQuiz;
+}
+
+export { cryptQuiz, decryptQuiz, displayQuiz };
